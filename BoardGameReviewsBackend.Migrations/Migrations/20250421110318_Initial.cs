1672778195d36e2a1d9b6a939563d1a77546b306
bogdangosa <bogdan.gosa@gmail.com>
2025-04-21 14:06:38 +0300

    made migrations, made setup for database

---
+++ BoardGameReviewsBackend.Migrations/Migrations/20250421110318_Initial.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace BoardGameReviewsBackend.Migrations.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Boardgames",
+                columns: table => new
+                {
+                    boardgameid = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Title = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    Category = table.Column<string>(type: "text", nullable: false),
+                    Image = table.Column<string>(type: "text", nullable: false),
+                    ReleaseDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    nrOfPlayers = table.Column<int>(type: "integer", nullable: true),
+                    playTime = table.Column<int>(type: "integer", nullable: true),
+                    weight = table.Column<int>(type: "integer", nullable: true),
+                    rating = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Boardgames", x => x.boardgameid);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Boardgames");
+        }
+    }
+}
