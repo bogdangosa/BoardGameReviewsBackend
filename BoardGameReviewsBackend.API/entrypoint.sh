e241c16f17d7d7451aac8c9dcc25df302bbc4145
bogdangosa <bogdan.gosa@gmail.com>
2025-05-28 23:28:51 +0300

    small fix?

--- BoardGameReviewsBackend.API/entrypoint.sh
+++ BoardGameReviewsBackend.API/entrypoint.sh
@@ -1,8 +1,3 @@
-#!/bin/bash
-set -e
-
-echo "Running EF Core migrations..."
-dotnet ef database update --project ../BoardGameReviewsBackend.Migrations --startup-project . --context ApplicationDbContext
-
-echo "Starting application..."
-exec dotnet BoardGameReviewsBackend.API.dll
+#!/bin/sh
+dotnet ef database update
+dotnet BoardGameReviewsBackend.API.dll
